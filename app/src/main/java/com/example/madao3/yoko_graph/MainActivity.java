ef1b648c64b36ec48896010e65647c338d718943
yappaakanwa <tamaki3517@gmail.com>
2015-06-20 13:44:53 +0900

    pointer 初期化

--- app/src/main/java/com/example/madao3/yoko_graph/MainActivity.java
+++ app/src/main/java/com/example/madao3/yoko_graph/MainActivity.java
@@ -4,14 +4,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ImageView pointer1;
+    ImageView pointer2;
+    ImageView pointer3;
+    ImageView pointer4;
+    ImageView pointer5;
+    ImageView pointer6;
+    ImageView pointer7;
+    ImageView pointer8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setPointer();
+    }
+
+    private void setPointer(){
+        pointer1 = (ImageView) findViewById(R.id.pointerImageView1);
+        pointer2 = (ImageView) findViewById(R.id.pointerImageView2);
+        pointer3 = (ImageView) findViewById(R.id.pointerImageView3);
+        pointer4 = (ImageView) findViewById(R.id.pointerImageView4);
+        pointer5 = (ImageView) findViewById(R.id.pointerImageView5);
+        pointer6 = (ImageView) findViewById(R.id.pointerImageView6);
+        pointer7 = (ImageView) findViewById(R.id.pointerImageView7);
+        pointer8 = (ImageView) findViewById(R.id.pointerImageView8);
     }
 
     @Override
